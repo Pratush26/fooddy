89d289f27680b2447bba53d85bc73963ab2e64a9
Pratush26 <pratushbarua26@gmail.com>
2026-01-27 23:46:10 +0600

    initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "@/providers/Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col justify-between max-h-screen`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
---
+++ providers/Provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+// import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "sonner";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+        {/* <SessionProvider> */}
+            {children}
+            <Toaster />
+        {/* </SessionProvider> */}
+        </ThemeProvider>
+    )
+}
